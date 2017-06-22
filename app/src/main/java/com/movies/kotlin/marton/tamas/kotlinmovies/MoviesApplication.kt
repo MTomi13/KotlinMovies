2281e11073a033ce00dae3d00da86631635d7737
Tamas Marton <tamas.marton@tuitravel.com>
2017-06-22 16:13:27 +0100

    create custom Application to store the api key

---
+++ app/src/main/java/com/movies/kotlin/marton/tamas/kotlinmovies/MoviesApplication.kt
@@ -0,0 +1,12 @@
+package com.movies.kotlin.marton.tamas.kotlinmovies
+
+import android.app.Application
+
+
+class MoviesApplication : Application() {
+
+    companion object {
+
+        const val API_KEY = "0a08e38b874d0aa2d426ffc04357069d"
+    }
+}
