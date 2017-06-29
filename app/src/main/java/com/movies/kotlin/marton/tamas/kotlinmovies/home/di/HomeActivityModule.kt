818b92626302f2505c34f268911861d4af92ae69
Tamas Marton <tamas.marton@tuitravel.com>
2017-06-29 14:11:39 +0100

    add imports to activity Modules

--- app/src/main/java/com/movies/kotlin/marton/tamas/kotlinmovies/home/di/HomeActivityModule.kt
+++ app/src/main/java/com/movies/kotlin/marton/tamas/kotlinmovies/home/di/HomeActivityModule.kt
@@ -1,15 +1,20 @@
 package com.movies.kotlin.marton.tamas.kotlinmovies.home.di
 
+import com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeActivity
+import com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeInteractorImpl
+import com.movies.kotlin.marton.tamas.kotlinmovies.home.HomePresenterImpl
+import com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeView
+
 @dagger.Module
 class HomeActivityModule {
 
     @dagger.Provides
-    fun provideHomeView(mainActivity: com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeActivity): com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeView {
+    fun provideHomeView(mainActivity: HomeActivity): HomeView {
         return mainActivity
     }
 
     @dagger.Provides
-    fun provideHomePresenter(homeView: com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeView, homeInteractorImpl: com.movies.kotlin.marton.tamas.kotlinmovies.home.HomeInteractorImpl): com.movies.kotlin.marton.tamas.kotlinmovies.home.HomePresenterImpl {
+    fun provideHomePresenter(homeView: HomeView, homeInteractorImpl: HomeInteractorImpl): HomePresenterImpl {
         return com.movies.kotlin.marton.tamas.kotlinmovies.home.HomePresenterImpl(homeInteractorImpl, homeView)
     }
 }
--- app/src/main/java/com/movies/kotlin/marton/tamas/kotlinmovies/splash/di/SplashActivityModule.kt
+++ app/src/main/java/com/movies/kotlin/marton/tamas/kotlinmovies/splash/di/SplashActivityModule.kt
@@ -1,20 +1,26 @@
 package com.movies.kotlin.marton.tamas.kotlinmovies.splash.di
 
+import com.movies.kotlin.marton.tamas.kotlinmovies.api.configuration.ConfigurationRequester
+import com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashActivity
+import com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashInteractor
+import com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashPresenter
+import com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashView
+
 @dagger.Module
 class SplashActivityModule {
 
     @dagger.Provides
-    fun provideSplashView(splashActivity: com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashActivity): com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashView {
+    fun provideSplashView(splashActivity: SplashActivity): SplashView {
         return splashActivity
     }
 
     @dagger.Provides
-    fun provideSplashPresenter(splashInteractor: com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashInteractor, splashView: com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashView): com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashPresenter {
+    fun provideSplashPresenter(splashInteractor: SplashInteractor, splashView: SplashView): SplashPresenter {
         return com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashPresenter(splashInteractor, splashView)
     }
 
     @dagger.Provides
-    fun provideSplashInteractor(configurationRequester: com.movies.kotlin.marton.tamas.kotlinmovies.api.configuration.ConfigurationRequester): com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashInteractor {
+    fun provideSplashInteractor(configurationRequester: ConfigurationRequester): SplashInteractor {
         return com.movies.kotlin.marton.tamas.kotlinmovies.splash.SplashInteractor(configurationRequester)
     }
 }
